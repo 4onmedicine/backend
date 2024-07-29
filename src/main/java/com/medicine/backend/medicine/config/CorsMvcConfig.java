3e7be1f0abf859230e440171a41c5fb6d99f139a
cjh-19 <gns8812@naver.com>
2024-07-29 17:57:57 +0900

    [Feat] Cors 설정

---
+++ src/main/java/com/medicine/backend/medicine/config/CorsMvcConfig.java
@@ -0,0 +1,19 @@
+package com.medicine.backend.medicine.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("https://4onmedicine.kro.kr")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}

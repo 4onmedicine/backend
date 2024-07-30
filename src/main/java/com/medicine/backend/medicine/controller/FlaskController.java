e06c76687bd1b313cad67992ddf442ecfe13b393
cjh-19 <gns8812@naver.com>
2024-07-30 15:48:04 +0900

    [Feat] flask에서 받은 보험 코드로 약정보 반환 코드 추가

--- src/main/java/com/medicine/backend/medicine/controller/FlaskController.java
+++ src/main/java/com/medicine/backend/medicine/controller/FlaskController.java
@@ -1,6 +1,8 @@
 package com.medicine.backend.medicine.controller;
 
 import com.medicine.backend.medicine.dto.ChatRequest;
+import com.medicine.backend.medicine.dto.ImgMedicineDetail;
+import com.medicine.backend.medicine.dto.MedicineDetail;
 import org.springframework.http.MediaType;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -13,8 +15,21 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class FlaskController {
@@ -46,6 +61,120 @@ public class FlaskController {
 
         // 이미지 전송
         ResponseEntity<String> response = restTemplate.exchange(serverUrl, HttpMethod.POST, requestEntity, String.class);
+        List<Integer> responseList = Arrays.asList(response.getBody().split(","))
+                .stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        /* 공공 데이터 약 낱알 api 호출 */
+        // 리스트로 저장
+        List<ImgMedicineDetail> resultList = new ArrayList<>();
+        for (Integer item : responseList) {
+            ImgMedicineDetail result = new ImgMedicineDetail();
+            try {
+                StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1471000/MdcinGrnIdntfcInfoService01/getMdcinGrnIdntfcInfoList01"); /*URL*/
+                urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=서비스키"); /*Service Key*/
+                urlBuilder.append("&" + URLEncoder.encode("edi_code", "UTF-8") + "=" + URLEncoder.encode(item.toString(), "UTF-8")); /*품목일련번호*/
+                urlBuilder.append("&" + URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8")); /*응답데이터 형식(xml/json) default : xml*/
+
+                URL url = new URL(urlBuilder.toString());
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Content-type", "application/json");
+                System.out.println("Response code: " + conn.getResponseCode());
+                BufferedReader rd;
+                if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                    rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                } else {
+                    rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                }
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    sb.append(line);
+                }
+                rd.close();
+                conn.disconnect();
+
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document doc = builder.parse(new java.io.ByteArrayInputStream(sb.toString().getBytes("UTF-8")));
+
+                NodeList itemNameList = doc.getElementsByTagName("ITEM_NAME");
+
+                if (itemNameList.getLength() > 0) {
+                    result.setItemName(itemNameList.item(0).getTextContent());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            resultList.add(result);
+
+        }
+
+        /* resultList 돌면서 약 정보 추출*/
+        List<MedicineDetail> resultMedicineList = new ArrayList<>();
+        for (ImgMedicineDetail imgMedicineDetail : resultList) {
+            MedicineDetail medicineDetail = null;
+            try {
+                StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList");
+                urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=46v4f%2FfTVSebPS3vIHjKErfM%2FNWA68Qlu%2FvYdzqhkNx%2B9aSeOLJ%2FFITCYliTwlqWIb%2F3%2BFIY27BrDYUQMuPdrA%3D%3D");
+                urlBuilder.append("&" + URLEncoder.encode("itemName", "UTF-8") + "=" + URLEncoder.encode(imgMedicineDetail.getItemName(), "UTF-8"));
+                urlBuilder.append("&" + URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8"));
+
+                URL url = new URL(urlBuilder.toString());
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Content-type", "application/json");
+                System.out.println("Response code: " + conn.getResponseCode());
+                BufferedReader rd;
+                if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                    rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                } else {
+                    rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                }
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    sb.append(line);
+                }
+                rd.close();
+                conn.disconnect();
+
+
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document doc = builder.parse(new java.io.ByteArrayInputStream(sb.toString().getBytes("UTF-8")));
+
+
+                NodeList itemSeqList = doc.getElementsByTagName("itemSeq");
+                NodeList itemNameList = doc.getElementsByTagName("itemName");
+                NodeList atpnQesitmList = doc.getElementsByTagName("atpnQesitm");
+                NodeList efcyQesitmList = doc.getElementsByTagName("efcyQesitm");
+                NodeList useMethodQesitmList = doc.getElementsByTagName("useMethodQesitm");
+                NodeList intrcQesitmList = doc.getElementsByTagName("intrcQesitm");
+                NodeList seQesitmList = doc.getElementsByTagName("seQesitm");
+                NodeList depositMethodQesitmList = doc.getElementsByTagName("depositMethodQesitm");
+                NodeList itemImageList = doc.getElementsByTagName("itemImage");
+
+
+                if (itemNameList.getLength() > 0) {
+                    medicineDetail = new MedicineDetail();
+                    medicineDetail.setItemSeq(Integer.parseInt(itemSeqList.item(0).getTextContent()));
+                    medicineDetail.setItemName(itemNameList.item(0).getTextContent());
+                    medicineDetail.setAtpnQesitm(atpnQesitmList.item(0).getTextContent());
+                    medicineDetail.setEfcyQesitm(efcyQesitmList.item(0).getTextContent());
+                    medicineDetail.setUseMethodQesitm(useMethodQesitmList.item(0).getTextContent());
+                    medicineDetail.setIntrcQesitm(intrcQesitmList.item(0).getTextContent());
+                    medicineDetail.setSeQesitm(seQesitmList.item(0).getTextContent());
+                    medicineDetail.setDepositMethodQesitm(depositMethodQesitmList.item(0).getTextContent());
+                    medicineDetail.setItemImage(itemImageList.item(0).getTextContent());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            resultMedicineList.add(medicineDetail);
+        }
 
         // Flask 서버로부터 받은 응답 반환
         return response.getBody();

c42361d018a81f8ade68cc8e7e12bedcdef855fe
cjh-19 <gns8812@naver.com>
2024-08-01 01:12:46 +0900

    [Feat] swagger https 설정 추가 Cors 오류 해결

--- src/main/java/com/medicine/backend/medicine/config/SwaggerConfig.java
+++ src/main/java/com/medicine/backend/medicine/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.medicine.backend.medicine.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,7 +15,8 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .components(new Components())
-                .info(apiInfo());
+                .info(apiInfo())
+                .servers(List.of(apiServer()));
     }
 
     private Info apiInfo() {
@@ -20,5 +24,11 @@ public class SwaggerConfig {
                 .title("Medicine API Documentation")
                 .description("springdoc Swagger UI 테스트")
                 .version("1.0.0");
+    }
+
+    private Server apiServer() {
+        Server server = new Server();
+        server.setUrl("https://4onmserver.kro.kr/");
+        return server;
     }
 }
